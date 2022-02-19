47535327966498eaccef67a37e8dff48832237c4
482F <482F.git.mail@482f.net>
2022-02-20 00:00:00 +0900

    シャッフルできるように

--- src/renderer/components/main.vue
+++ src/renderer/components/main.vue
@@ -4,8 +4,10 @@
       :music="current.music"
       :volume="volume"
       @update:volume="updateVolume"
+      @shuffle="shuffle"
     />
     <music-lists
+      ref="musicLists"
       class="music-lists"
       :lists="info.lists"
       @update:lists="updateLists"
@@ -20,6 +22,19 @@ import CustomAudio from '@/renderer/classes/custom-audio.js'
 import Info from '@/renderer/classes/info.js'
 import MusicLists from './music-lists.vue'
 import Player from './player.vue'
+
+function shuffle(arr) {
+  const rands = Array(arr.length)
+    .fill(0)
+    .map(() => Math.random())
+  const randAndIndice = rands.map((rand, i) => [rand, i])
+  const randIndice = randAndIndice
+    .sort(([rand1], [rand2]) => rand1 - rand2)
+    .map(([_, i]) => i)
+  const result = []
+  randIndice.forEach((index) => result.push(arr[index]))
+  return result
+}
 
 export default {
   name: 'main',
@@ -39,6 +54,14 @@ export default {
     await this.initInfo()
   },
   methods: {
+    shuffle() {
+      this.$refs.musicLists.currentList.musics = shuffle(
+        this.$refs.musicLists.currentList.musics
+      )
+      this.current.index = this.$refs.musicLists.currentList.musics.findIndex(
+        (music) => music.isPlaying
+      )
+    },
     async initInfo() {
       this.info = new Info()
       this.info.import('E:\\info.mp5')
@@ -66,18 +89,18 @@ export default {
     async open(music, list, index) {
       this.current.list = list
       this.current.index = index
+      list.musics[index].isPlaying = true
       this.current.music?.stop?.()
       this.current.music = await CustomAudio.construct(music.path, this.volume)
       this.current.music.start()
       this.current.music.onended = () => this.onended()
     },
     async onended() {
+      const nextIndex = this.current.index + 1
       await this.open(
-        this.current.list.musics[
-          this.current.index % this.current.list.musics.length
-        ],
+        this.current.list.musics[nextIndex % this.current.list.musics.length],
         this.current.list,
-        this.current.index + 1
+        nextIndex
       )
     },
   },
