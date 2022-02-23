ac2b67a4515d968ba4936c9a7ad73fc342bbbbc5
482F <482F.git.mail@482f.net>
2022-02-24 00:00:00 +0900

    色分けされた歌詞データを表示できるように

--- src/renderer/components/lrc/lrc-show.vue
+++ src/renderer/components/lrc/lrc-show.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="lrc-show" :style="{ '--line-height': lineHeight + 'px' }">
     <div class="padding" />
-    <div class="body" :style="{ '--offset': offset + 'px' }">
+    <div class="body">
       <div
-        v-for="({ text: line }, i) of lyricData"
+        class="column"
+        v-for="(column, i) of lyricData"
         :key="i"
-        class="line"
-        :class="{ current: currentIndex === i }"
+        :style="{ '--offset': offsets[i] + 'px' }"
       >
-        {{ line }}
+        <div
+          v-for="({ text: line, color }, j) of column"
+          :key="j"
+          class="line"
+          :class="{ current: currentIndice[i] === j }"
+          :style="{ color }"
+        >
+          {{ line }}
+        </div>
       </div>
     </div>
   </div>
@@ -37,19 +45,25 @@ export default {
     currentTime() {
       return Math.round((this.music?.currentTime ?? 0) * 1000)
     },
-    currentIndex() {
-      const currentIndex =
-        this.lyricData.findIndex(({ time }) => this.currentTime < time) - 1
-      return currentIndex === -2 ? this.lyricData.length - 1 : currentIndex
+    currentIndice() {
+      return this.lyricData.map((column) => {
+        const currentIndex =
+          column.findIndex(({ time }) => this.currentTime < time) - 1
+        return currentIndex === -2 ? column.length - 1 : currentIndex
+      })
     },
-    offset() {
-      if (this.currentIndex === -1) {
-        return 0
-      }
-      const partStart = this.lyricData[this.currentIndex].time
-      const partEnd = this.lyricData[this.currentIndex + 1]?.time ?? Infinity
-      const decimalPart = (this.currentTime - partStart) / (partEnd - partStart)
-      return Math.round((this.currentIndex + decimalPart) * lineHeight)
+    offsets() {
+      return this.lyricData.map((column, i) => {
+        const currentIndex = this.currentIndice[i]
+        if (currentIndex === -1) {
+          return 0
+        }
+        const partStart = column[currentIndex].time
+        const partEnd = column[currentIndex + 1]?.time ?? Infinity
+        const decimalPart =
+          (this.currentTime - partStart) / (partEnd - partStart)
+        return Math.round((currentIndex + decimalPart) * lineHeight)
+      })
     },
   },
 }
@@ -64,12 +78,23 @@ export default {
     height: 50%;
   }
   > .body {
-    margin-top: calc(-1 * var(--offset));
-    > .line {
-      height: var(--line-height);
-      color: #888;
-      &.current {
-        color: black;
+    display: flex;
+    > .column {
+      flex-grow: 1;
+      margin-top: calc(-1 * var(--offset));
+      position: relative;
+      > .line {
+        height: var(--line-height);
+        &:not(.current) {
+          &::after {
+            content: '';
+            width: 100%;
+            height: var(--line-height);
+            position: absolute;
+            left: 0;
+            background-color: #ffffff77;
+          }
+        }
       }
     }
   }
