faf2d49af865a28e9566c56d748e2e68c043a02f
482F <0ryonryoko0@gmail.com>
2022-02-23 10:44:05 +0900

    リストの項目をコンポーネント化

---
+++ src/renderer/components/music-item.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="music-item">
+    <v-list-item>
+      <div class="playing-icon">
+        <v-icon v-show="music.isPlaying">mdi-play</v-icon>
+      </div>
+      <div>{{ music.name }}</div>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'music-item',
+  props: {
+    music: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.music-item {
+  .playing-icon {
+    width: 32px;
+  }
+}
+</style>
